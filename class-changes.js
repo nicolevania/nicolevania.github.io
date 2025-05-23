f41cd45d6bd5ea082e257ba8fa015de3f55fac55
nicolevania <nicolewinarta@gmail.com>
2025-05-23 10:48:29 +1000

    fourth upload

---
+++ class-changes.js
@@ -0,0 +1,16 @@
+const classChanges = [
+  { time: 5, target: ".hugeblurtext", add: "cream" },
+  { time: 7, target: ".hugeblurtext", remove: "cream" },
+  { time: 9, target: ".page", add: "update" },
+  { time: 12, target: ".page", remove: "update" },
+
+  { time: 9, target: ".popup", add: "hide" },
+  { time: 16, target: ".popup", remove: "hide" },
+  { time: 16, target: ".page", add: "update" },
+  { time: 19, target: ".page", remove: "update" },
+
+
+  { time: 16, target: ".changeme3", add: "show" },
+  { time: 28, target: ".changeme3", remove: "show" },
+  { time: 28, target: ".changeme3", add: "hidden" }
+];
